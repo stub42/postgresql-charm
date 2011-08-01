52aad58c34de27d37ebac8f28593aa9a6c140f3f
Mark Mims <mark.mims@canonical.com>
2011-08-01 17:23:08 -0600

    admin relation

--- hooks/common.sh
+++ hooks/common.sh
@@ -1,5 +1,19 @@
 
+
 database=${ENSEMBLE_REMOTE_UNIT///*}
 user=${ENSEMBLE_REMOTE_UNIT///*}
 host=`hostname -f`
+
+admin() {
+  [[ $(basename $0) =~ /admin/ ]]
+}
+
+database_name() {
+  if admin; then
+    "all"
+  else
+    ${database}
+  fi
+}
+
 
