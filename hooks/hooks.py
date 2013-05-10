9763b1969fe4a860a5415695dee3989172413c4a
Andreas Hasenack <andreas@canonical.com>
2013-05-10 16:35:58 -0700

    Handle "null" json output

--- hooks/hooks.py
+++ hooks/hooks.py
@@ -468,7 +468,9 @@ def relation_list(relation_id=None):
     cmd = ['relation-list', '--format=json', '-r', relation_id]
     json_units = subprocess.check_output(cmd).strip()
     if json_units:
-        return json.loads(json_units)
+        data = json.loads(json_units)
+        if data is not None:
+            return data
     return []
 
 
