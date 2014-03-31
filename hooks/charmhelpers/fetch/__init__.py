d0af262cd82adff86e469decefab92217180e505
Stuart Bishop <stuart.bishop@canonical.com>
2014-03-31 19:37:26 +0700

    Update charm-helpers from trunk

--- hooks/charmhelpers/fetch/__init__.py
+++ hooks/charmhelpers/fetch/__init__.py
@@ -13,6 +13,7 @@ from charmhelpers.core.hookenv import (
     log,
 )
 import apt_pkg
+import os
 
 CLOUD_ARCHIVE = """# Ubuntu Cloud Archive
 deb http://ubuntu-cloud.archive.canonical.com/ubuntu {} main
@@ -20,6 +21,40 @@ deb http://ubuntu-cloud.archive.canonical.com/ubuntu {} main
 PROPOSED_POCKET = """# Proposed
 deb http://archive.ubuntu.com/ubuntu {}-proposed main universe multiverse restricted
 """
+CLOUD_ARCHIVE_POCKETS = {
+    # Folsom
+    'folsom': 'precise-updates/folsom',
+    'precise-folsom': 'precise-updates/folsom',
+    'precise-folsom/updates': 'precise-updates/folsom',
+    'precise-updates/folsom': 'precise-updates/folsom',
+    'folsom/proposed': 'precise-proposed/folsom',
+    'precise-folsom/proposed': 'precise-proposed/folsom',
+    'precise-proposed/folsom': 'precise-proposed/folsom',
+    # Grizzly
+    'grizzly': 'precise-updates/grizzly',
+    'precise-grizzly': 'precise-updates/grizzly',
+    'precise-grizzly/updates': 'precise-updates/grizzly',
+    'precise-updates/grizzly': 'precise-updates/grizzly',
+    'grizzly/proposed': 'precise-proposed/grizzly',
+    'precise-grizzly/proposed': 'precise-proposed/grizzly',
+    'precise-proposed/grizzly': 'precise-proposed/grizzly',
+    # Havana
+    'havana': 'precise-updates/havana',
+    'precise-havana': 'precise-updates/havana',
+    'precise-havana/updates': 'precise-updates/havana',
+    'precise-updates/havana': 'precise-updates/havana',
+    'havana/proposed': 'precise-proposed/havana',
+    'precise-havana/proposed': 'precise-proposed/havana',
+    'precise-proposed/havana': 'precise-proposed/havana',
+    # Icehouse
+    'icehouse': 'precise-updates/icehouse',
+    'precise-icehouse': 'precise-updates/icehouse',
+    'precise-icehouse/updates': 'precise-updates/icehouse',
+    'precise-updates/icehouse': 'precise-updates/icehouse',
+    'icehouse/proposed': 'precise-proposed/icehouse',
+    'precise-icehouse/proposed': 'precise-proposed/icehouse',
+    'precise-proposed/icehouse': 'precise-proposed/icehouse',
+}
 
 
 def filter_installed_packages(packages):
@@ -40,8 +75,10 @@ def filter_installed_packages(packages):
 
 def apt_install(packages, options=None, fatal=False):
     """Install one or more packages"""
-    options = options or []
-    cmd = ['apt-get', '-y']
+    if options is None:
+        options = ['--option=Dpkg::Options::=--force-confold']
+
+    cmd = ['apt-get', '--assume-yes']
     cmd.extend(options)
     cmd.append('install')
     if isinstance(packages, basestring):
@@ -50,10 +87,37 @@ def apt_install(packages, options=None, fatal=False):
         cmd.extend(packages)
     log("Installing {} with options: {}".format(packages,
                                                 options))
+    env = os.environ.copy()
+    if 'DEBIAN_FRONTEND' not in env:
+        env['DEBIAN_FRONTEND'] = 'noninteractive'
+
     if fatal:
-        subprocess.check_call(cmd)
+        subprocess.check_call(cmd, env=env)
     else:
-        subprocess.call(cmd)
+        subprocess.call(cmd, env=env)
+
+
+def apt_upgrade(options=None, fatal=False, dist=False):
+    """Upgrade all packages"""
+    if options is None:
+        options = ['--option=Dpkg::Options::=--force-confold']
+
+    cmd = ['apt-get', '--assume-yes']
+    cmd.extend(options)
+    if dist:
+        cmd.append('dist-upgrade')
+    else:
+        cmd.append('upgrade')
+    log("Upgrading with options: {}".format(options))
+
+    env = os.environ.copy()
+    if 'DEBIAN_FRONTEND' not in env:
+        env['DEBIAN_FRONTEND'] = 'noninteractive'
+
+    if fatal:
+        subprocess.check_call(cmd, env=env)
+    else:
+        subprocess.call(cmd, env=env)
 
 
 def apt_update(fatal=False):
@@ -67,7 +131,7 @@ def apt_update(fatal=False):
 
 def apt_purge(packages, fatal=False):
     """Purge one or more packages"""
-    cmd = ['apt-get', '-y', 'purge']
+    cmd = ['apt-get', '--assume-yes', 'purge']
     if isinstance(packages, basestring):
         cmd.append(packages)
     else:
@@ -79,22 +143,49 @@ def apt_purge(packages, fatal=False):
         subprocess.call(cmd)
 
 
+def apt_hold(packages, fatal=False):
+    """Hold one or more packages"""
+    cmd = ['apt-mark', 'hold']
+    if isinstance(packages, basestring):
+        cmd.append(packages)
+    else:
+        cmd.extend(packages)
+    log("Holding {}".format(packages))
+    if fatal:
+        subprocess.check_call(cmd)
+    else:
+        subprocess.call(cmd)
+
+
 def add_source(source, key=None):
-    if ((source.startswith('ppa:') or
-         source.startswith('http:'))):
+    if source is None:
+        log('Source is not present. Skipping')
+        return
+
+    if (source.startswith('ppa:') or
+        source.startswith('http') or
+        source.startswith('deb ') or
+            source.startswith('cloud-archive:')):
         subprocess.check_call(['add-apt-repository', '--yes', source])
     elif source.startswith('cloud:'):
         apt_install(filter_installed_packages(['ubuntu-cloud-keyring']),
                     fatal=True)
         pocket = source.split(':')[-1]
+        if pocket not in CLOUD_ARCHIVE_POCKETS:
+            raise SourceConfigError(
+                'Unsupported cloud: source option %s' %
+                pocket)
+        actual_pocket = CLOUD_ARCHIVE_POCKETS[pocket]
         with open('/etc/apt/sources.list.d/cloud-archive.list', 'w') as apt:
-            apt.write(CLOUD_ARCHIVE.format(pocket))
+            apt.write(CLOUD_ARCHIVE.format(actual_pocket))
     elif source == 'proposed':
         release = lsb_release()['DISTRIB_CODENAME']
         with open('/etc/apt/sources.list.d/proposed.list', 'w') as apt:
             apt.write(PROPOSED_POCKET.format(release))
     if key:
-        subprocess.check_call(['apt-key', 'import', key])
+        subprocess.check_call(['apt-key', 'adv', '--keyserver',
+                               'keyserver.ubuntu.com', '--recv',
+                               key])
 
 
 class SourceConfigError(Exception):
@@ -117,9 +208,12 @@ def configure_sources(update=False,
 
     Note that 'null' (a.k.a. None) should not be quoted.
     """
-    sources = safe_load(config(sources_var)) or []
-    keys = safe_load(config(keys_var)) or []
-    if isinstance(sources, basestring) and isinstance(keys, basestring):
+    sources = safe_load(config(sources_var))
+    keys = config(keys_var)
+    if keys is not None:
+        keys = safe_load(keys)
+    if isinstance(sources, basestring) and (
+            keys is None or isinstance(keys, basestring)):
         add_source(sources, keys)
     else:
         if not len(sources) == len(keys):
@@ -172,7 +266,9 @@ def install_from_config(config_var_name):
 
 
 class BaseFetchHandler(object):
+
     """Base class for FetchHandler implementations in fetch plugins"""
+
     def can_handle(self, source):
         """Returns True if the source can be handled. Otherwise returns
         a string explaining why it cannot"""
@@ -200,10 +296,13 @@ def plugins(fetch_handlers=None):
     for handler_name in fetch_handlers:
         package, classname = handler_name.rsplit('.', 1)
         try:
-            handler_class = getattr(importlib.import_module(package), classname)
+            handler_class = getattr(
+                importlib.import_module(package),
+                classname)
             plugin_list.append(handler_class())
         except (ImportError, AttributeError):
             # Skip missing plugins so that they can be ommitted from
             # installation if desired
-            log("FetchHandler {} not found, skipping plugin".format(handler_name))
+            log("FetchHandler {} not found, skipping plugin".format(
+                handler_name))
     return plugin_list
