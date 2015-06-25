8fe8a19e19a4dd95e6946b60e81afef7b11d19b5
Stuart Bishop <stuart.bishop@canonical.com>
2015-06-25 18:35:33 +0700

    wip

--- actions/actions.py
+++ actions/actions.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python
 # Copyright 2015 Canonical Ltd.
 #
 # This file is part of the PostgreSQL Charm for Juju.
@@ -17,24 +18,61 @@ import os.path
 import sys
 import traceback
 
+
+hooks_dir = os.path.abspath(os.path.join(os.path.dirname(__file__),
+                                         '..', 'hooks'))
+if hooks_dir not in sys.path:
+    sys.path.append(hooks_dir)
+
 from charmhelpers.core import hookenv
+import hooks
 
 
-def replication(params):
-    hookenv.action_set(result=True)
+def replication_pause(params):
+    offset = hooks.postgresql_wal_received_offset()
+    if offset is None:
+        hookenv.action_fail('Not a hot standby')
+        return
+    hookenv.action_set(dict(offset=offset))
+
+    cur = hooks.db_cursor(autocommit=True)
+    cur.execute('SELECT pg_is_xlog_replay_paused()')
+    if cur.fetchone()[0] is True:
+        hookenv.action_fail('Already paused')
+        return
+    cur.execute('SELECT pg_xlog_replay_pause()')
+    hookenv.action_set(dict(result='Paused'))
+
+
+def replication_resume(params):
+    offset = hooks.postgresql_wal_received_offset()
+    if offset is None:
+        hookenv.action_fail('Not a hot standby')
+        return
+    hookenv.action_set(dict(offset=offset))
+
+    cur = hooks.db_cursor(autocommit=True)
+    cur.execute('SELECT pg_is_xlog_replay_paused()')
+    if cur.fetchone()[0] is False:
+        hookenv.action_fail('Already resumed')
+        return
+    cur.execute('SELECT pg_xlog_replay_resume()')
+    hookenv.action_set(dict(result='Resumed'))
 
 
 def main(argv):
     action = os.path.basename(argv[0])
     params = hookenv.action_get()
     try:
-        if action == 'replication':
-            replication(params)
+        if action == 'replication-pause':
+            replication_pause(params)
+        elif action == 'replication-resume':
+            replication_resume(params)
         else:
             hookenv.action_fail('Action {} not implemented'.format(action))
     except Exception:
         hookenv.action_fail('Unhandled exception')
-        hookenv.action_set(traceback=traceback.format_exc())
+        hookenv.action_set(dict(traceback=traceback.format_exc()))
 
 
 if __name__ == '__main__':
