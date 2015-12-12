ff53dd91dd677c7729080dde2cf8ece60e71c523
Stuart Bishop <stuart.bishop@canonical.com>
2015-12-12 15:19:15 +0700

    Syslog relation

---
+++ hooks/relations/syslog/provides.py
@@ -0,0 +1,73 @@
+# Copyright 2015 Canonical Ltd.
+#
+# This file is part of the PostgreSQL Charm for Juju.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License version 3, as
+# published by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranties of
+# MERCHANTABILITY, SATISFACTORY QUALITY, or FITNESS FOR A PARTICULAR
+# PURPOSE.  See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import os.path
+
+from charmhelpers.core import hookenv, host, templating
+
+from charms import reactive
+from charms.reactive import hook, not_unless, when
+
+
+class SyslogProvides(reactive.relations.RelationBase):
+
+    class states(reactive.relations.StateList):
+        available = reactive.State('{relation_name}.available')
+
+    @hook('{provides:syslog}-relation-changed')
+    def changed(self):
+        self.set_state('{relation_name}.available')
+
+    @hook('{provides:syslog}-relation-departed')
+    def departed(self):
+        path = self.get_local('path')
+        if os.path.exists(path):
+            os.remove(path)
+            reactive.set_state('syslog.needs_restart')
+        self.remove_state('{relation_name}.available')
+        self.depart()
+
+    def _rsyslog_conf_path(self, remote_unit):
+        # Use both the local unit and remote unit in the config file
+        # path to avoid conflicts with subordinates.
+        rsyslog_conf_dir = '/etc/rsyslog.d'
+        local = hookenv.local_unit().replace('/', '_')
+        remote = remote_unit.replace('/', '_')
+        return os.path.join(rsyslog_conf_dir,
+                            'juju-{}-{}.conf'.format(local, remote))
+
+    @not_unless('{provides:syslog}.available')
+    def configure(self, programname):
+        for conv in self.conversations():
+            remote_unit = conv.scope
+            remote_addr = conv.get_remote('private-address')
+
+            if reactive.helpers.data_changed('syslog.{}'.format(remote_unit),
+                                             (remote_unit, remote_addr)):
+                path = self._rsyslog_conf_path(remote_unit)
+
+                templating.render('rsyslog_forward.conf', path,
+                                  dict(local_unit=hookenv.local_unit(),
+                                       remote_unit=remote_unit,
+                                       remote_addr=remote_addr,
+                                       programname=programname))
+                reactive.set_state('syslog.needs_restart')
+
+
+@when('syslog.needs_restart')
+def restart_rsyslog():
+    host.service_restart('rsyslog')
+    reactive.remove_state('syslog.needs_restart')
---
+++ reactive/postgresql/syslog.py
@@ -0,0 +1,38 @@
+# Copyright 2015 Canonical Ltd.
+#
+# This file is part of the PostgreSQL Charm for Juju.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License version 3, as
+# published by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranties of
+# MERCHANTABILITY, SATISFACTORY QUALITY, or FITNESS FOR A PARTICULAR
+# PURPOSE.  See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+from charmhelpers.core import hookenv, unitdata
+from charms.reactive import when
+
+
+@when('syslog.available', 'postgresql.cluster.configured')
+def configure_syslog(syslog):
+    programname = hookenv.local_unit().replace('/', '_')
+    syslog.configure(programname=programname)
+
+    # Extend the basic syslog interface. Add programname and
+    # log_line_prefix, required for consumers such as pgBadger to
+    # decode the logs.
+    log_line_prefix = get_log_line_prefix()
+    for conv in syslog.conversations():
+        conv.set_remote('programname', programname)
+        conv.set_remote('log_line_prefix', log_line_prefix)
+
+
+def get_log_line_prefix():
+    store = unitdata.kv()
+    log_line_prefix_key = 'postgresql.cluster.pgconf.current.log_line_prefix'
+    return store.get(log_line_prefix_key)
