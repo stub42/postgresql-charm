c4a2de4294c16df50d77b81da8f4ca3e12c7e99f
Stuart Bishop <stuart.bishop@canonical.com>
2016-02-04 14:58:16 +0700

    Switch to standard hooks

--- hooks/defaulthook.py
+++ hooks/defaulthook.py
@@ -1,67 +1,18 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 
-# Copyright 2015 Canonical Ltd.
-#
-# This file is part of the PostgreSQL Charm for Juju.
-#
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License version 3, as
-# published by the Free Software Foundation.
-#
-# This program is distributed in the hope that it will be useful, but
-# WITHOUT ANY WARRANTY; without even the implied warranties of
-# MERCHANTABILITY, SATISFACTORY QUALITY, or FITNESS FOR A PARTICULAR
-# PURPOSE.  See the GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
-import os.path
+# Load modules from $CHARM_DIR/lib
 import sys
-
-ROOT = os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir))
-sys.path.insert(1, ROOT)
-sys.path.insert(2, os.path.join(ROOT, 'lib'))
+sys.path.append('lib')
 
 from charms.bootstrap import bootstrap_charm_deps
 bootstrap_charm_deps()
 
-from charmhelpers import fetch  # NOQA: flake8
-from charmhelpers.core import hookenv  # NOQA: flake8
-from charmhelpers.core.hookenv import WARNING  # NOQA: flake8
-from charms.reactive import main  # NOQA: flake8
 
-# Work around https://github.com/juju-solutions/charms.reactive/issues/33
-import reactive.apt  # NOQA: flake8
-import reactive.workloadstatus  # NOQA: flake8
-import preflight  # NOQA: flake8
-import everyhook  # NOQA: flake8
-
-
-def bootstrap():
-    try:
-        import psycopg2  # NOQA: flake8
-        import jinja2  # NOQA: flake8
-    except ImportError:
-        packages = ['python3-psycopg2', 'python3-jinja2']
-        fetch.apt_install(packages, fatal=True)
-        import psycopg2  # NOQA: flake8
-        import jinja2  # NOQA: flake8
-
-
-def default_hook():
-    hookenv.log('*** Start {!r} hook'.format(hookenv.hook_name()))
-    bootstrap()
-
-    # Kick off the charms.reactive reactor.
-    try:
-        main()
-        hookenv.log('*** {!r} hook completed'.format(hookenv.hook_name()))
-    except SystemExit as x:
-        hookenv.log('*** {!r} hook aborted code {}'.format(hookenv.hook_name(),
-                                                           x.code), WARNING)
-        raise
-
-
-if __name__ == '__main__':
-    default_hook()
+# This will load and run the appropriate @hook and other decorated
+# handlers from $CHARM_DIR/reactive, $CHARM_DIR/hooks/reactive,
+# and $CHARM_DIR/hooks/relations.
+#
+# See https://jujucharms.com/docs/stable/getting-started-with-charms-reactive
+# for more information on this pattern.
+from charms.reactive import main
+main()
