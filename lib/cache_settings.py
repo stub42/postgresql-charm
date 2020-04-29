dedeb6ab4e5e1ed41c51962640127167c15cf0f4
Stuart Bishop <stuart.bishop@canonical.com>
2020-04-29 21:09:39 +1000

    Reformat PG version settings cache script

--- lib/cache_settings.py
+++ lib/cache_settings.py
@@ -31,20 +31,14 @@ ver = ver.split(".")[0]
 cur = con.cursor(cursor_factory=psycopg2.extras.RealDictCursor)
 cur.execute(
     """
-            SELECT name, unit, category, short_desc, extra_desc,
-                   context, vartype, min_val, max_val, enumvals,
-                   boot_val
-            FROM pg_settings
-            WHERE context <> 'internal'
-            """
+    SELECT name, unit, category, short_desc, extra_desc,
+            context, vartype, min_val, max_val, enumvals,
+            boot_val
+    FROM pg_settings
+    WHERE context <> 'internal'
+    """
 )
 
 cache = os.path.join(os.path.dirname(__file__), "pg_settings_{}.json".format(ver))
 with open(cache, "w") as f:
-    json.dump(
-        {d["name"].lower(): d for d in cur.fetchall()},
-        f,
-        ensure_ascii=True,
-        indent=4,
-        sort_keys=True,
-    )
+    json.dump({d["name"].lower(): d for d in cur.fetchall()}, f, ensure_ascii=True, indent=4, sort_keys=True)
